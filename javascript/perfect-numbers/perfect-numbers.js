1e58906428723f3c91a96a7d90fcd9ddbcb7ead9
William Purcell <william@chartbeat.com>
2022-03-01 16:16:25 -0500

    starting perfect numbers

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -1,8 +1,19 @@
-//
-// This is only a SKELETON file for the 'Perfect Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const classify = () => {
-  throw new Error('Remove this statement and implement this function');
+// function that tests whether a number is Perfect, Abundant, or Deficient according
+// to the classifications of Niomachus and his classification scheme for perfect
+// integers.
+
+// our function to classify the numbers
+// accepts: an integer
+// returns: a classification
+export const classify = (number) => {
+  
+  // check to ensure the number is positive and non-zero
+  if (number <= 0) {
+    throw Error('Classification is only possible for natural numbers.');
+  } 
+  // otherwise, perform the check
+  else {
+
+  }
 };
