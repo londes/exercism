b4f984cf69bb0fc0b326a741e614ac68912eca08
londes <wedozeez@gmail.com>
2022-03-07 14:16:22 -0500

    we fixed the broken check for all possible factors but we still need to fix adding the number itself to the array, and prevent duplicates

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -30,19 +30,24 @@ export const classify = (number) => {
   let iterations = 0;
 
   // we have to store the starting number passed into classify to deal with prime numbers
-  // in our factors algorithm.
+  // in our factors algorithm. This is just for debugging purposes and can be removed if
+  // desired.
   let startingNumber = number;
 
   // the sqrt of the original number is the highest common factor of that number.
   // so, we want to test if all of the ints between 2 and the sqrt factor into
   // that number to account for all possible factors.
-  let possibleFactors = getLowestPossibleFactors();
+  let possibleFactors = getLowestPossibleFactors(number);
   console.log('the possible lowest common factors of ' + number + ' are ' + possibleFactors);
 
-  // build our array of numbers that are factors of the number passed to classify(). we need to give
-  // the array to recursively check each possible factor.
-  let numberFactors = getFactors(number, startingNumber);
-  console.log('we called getFactors and the factors of ' + number + ' are ' + numberFactors);
+  // for each possible factor, check if it is a factor of our starting number. If so, recursively
+  // check for all numbers that also divide into our starting number that may be factors
+  // of that possible factor.
+  possibleFactors.forEach(possibleFactor => {
+    getFactors(number, possibleFactor, startingNumber);
+  });
+  //let numberFactors = getFactors(number, startingNumber);
+  console.log('we called getFactors and the factors of ' + number + ' are ' + factors);
   
   // get the total of all of the factors
   let factorsTotal = numberFactors.reduce((first, next) => first + next);
@@ -63,41 +68,34 @@ export const classify = (number) => {
   // of its factors.
   // accepts: a number, and our number again to store it as a starting value
   // returns: an array of factors
-  function getFactors(number, startingNumber) {
+  function getFactors(number, possibleFactor, startingNumber) {
     iterations++;
     console.log('our starting number is: ' + startingNumber);
     console.log('we called getFactors() on ' + number + ' and this is iteration ' + iterations);
     console.log('is ' + number + ' prime? ' + isPrime(number));
 
-    possibleFactors.forEach(int => {
-      console.log('we are in the possibleFactors forEach and we\'re evaluating ' + int );
-      if (number % int == 0) {
-        console.log(number + ' is divisible by '+ int + ' and we\'re in the ' + int + ' loop');
-        // if the number is 2, we've reached the end of our recursive loop. we want
-        // to return the completed array at this point.
-        if (number == int) {
-          factors.push(int);    // if the number is divisible by a possible factor, store the number and run this
-        // algorithm again
-        } 
-        // if our possible factor is larger than the number we are evaluating, do nothing
-        if (int > number) {  
-        } else {
-          factors.push(number/int);
-          getFactors(number/int, startingNumber);
-        }
+    if (number % possibleFactor == 0) {
+      console.log(number + ' is divisible by '+ possibleFactor + ' and we\'re in the ' + possibleFactor + ' loop');
+      // if the number is 2, we've reached the end of our recursive loop. we want
+      // to return the completed array at this popossibleFactor.
+      if (number == possibleFactor) {
+        factors.push(possibleFactor);    // if the number is divisible by a possible factor, store the number and run this
+      // algorithm again
+      } 
+      // if our possible factor is larger than the number we are evaluating, do nothing
+      if (possibleFactor > number) {  
+      } else {
+        factors.push(number/possibleFactor);
+        console.log('we just pushed ' + number/possibleFactor);
+        getFactors(number/possibleFactor, possibleFactor, startingNumber);
       }
-    })
-
+    }
     
     // these two cases cover when the last number we arrive at is prime, and not divisible
     // by two or three. This way, we can still get 2 and/or 3 as a factor by checking
     // against the original number
-    if (isPrime(number) && (startingNumber % 2 == 0)) {
-      factors.push(2);
-    }
-
-    if (isPrime(number) && (startingNumber % 3 == 0)) {
-      factors.push(3);
+    if (isPrime(number) && (startingNumber % number == 0)) {
+      factors.push(number);
     }
 
     // reset iterations to 0 since we're done here with this run-thru
@@ -145,12 +143,12 @@ export const classify = (number) => {
   // function that constructs an array of all possible lowest factors of a number.
   // The highest common factor of any number must be its square root. Therefore,
   // we should check each number leading up to its square root in our check.
-  function getLowestPossibleFactors () {
-      let getFactors = [];
+  function getLowestPossibleFactors (number) {
+      let lowestPossibleFactors = [];
       for(let i=2, s = Math.sqrt(number); i <= s; i++){
-        getFactors.push(i);
+        lowestPossibleFactors.push(i);
       }
-      return getFactors;
+      return lowestPossibleFactors;
   }
 
 // our algorithm fails if after dividing down into a number's factors, it ends on a prime number.
