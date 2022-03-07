fc3d9a62a57efb0e22e9637a65d5b086be89a149
William Purcell <william@chartbeat.com>
2022-03-06 23:58:01 -0500

    to add contains() to account for duplicate factors being used

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -50,6 +50,7 @@ export const classify = (number) => {
   } else return 'deficient';
 
   // implement recursive check for each integer between 2 and sqrt of number
+  // pass array in and potentially use contains() to check for duplicates
 
   // getFactors is a recursive algorithm that takes a number and returns an array of all
   // of its factors.
