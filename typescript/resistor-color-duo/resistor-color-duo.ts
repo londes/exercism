fea4633455b3f34386d8a8b9e2ee6dc9369228fc
William Purcell <william@chartbeat.com>
2022-03-22 20:40:51 -0400

    committing progress before getting beer and dinner

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,70 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
-}
+export function decodedValue(decodedValue: Array<string>) {
+  type Band = {
+    color: string;
+    colorValue: number;
+  }
+
+  let bandsArray: Array<Band> = [
+    {
+      color: 'black',
+      colorValue: 0
+    },
+    {
+      color: 'brown',
+      colorValue: 0
+    },
+    {
+      color: 'red',
+      colorValue: 0
+    },
+    {
+      color: 'orange',
+      colorValue: 0
+    },
+    {
+      color: 'yellow',
+      colorValue: 0
+    },
+    {
+      color: 'green',
+      colorValue: 0
+    },
+    {
+      color: 'blue',
+      colorValue: 0
+    },
+    {
+      color: 'violet',
+      colorValue: 0
+    },
+    {
+      color: 'grey',
+      colorValue: 0
+    },
+    {
+      color: 'white',
+      colorValue: 0
+    },
+  ]
+
+  let colorValues: Array<number> = [];
+
+  // let valuesArray = decodedValue.forEach(value => {
+  //   bandsArray.filter(band => {
+  //     return band.color == value;
+  //   }).map(() => {
+  //     return band.colorValue;
+  //   })
+  // });
+
+  // return valuesArray;
+
+  // function getValues(decoded: Array<string>, bands: Array<Band>) {
+  //   return bands.filter(band => {
+  //     return decoded.includes(band.color);
+  //   }).map(band => band.colorValue);
+  // }
+};
+
+
+
