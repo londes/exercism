23cd83b6303c4ad50dbd975d529cc78c2081c375
William Purcell <william@chartbeat.com>
2022-03-25 18:22:58 -0400

    leap year done

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,5 @@
 export function isLeap(year: number) {
-  return (year % 4 === 0);
+
+  // return true if the year is divisible by 4, and it is not divisible by 100 unless it's also divisible by 400
+  return ((year % 4 === 0) && ((year % 100 !== 0) || (year % 400 === 0)));
 }
