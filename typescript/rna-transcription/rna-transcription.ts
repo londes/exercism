261f532f7ca93db5009037323ecf96fcefa8583d
William Purcell <william@chartbeat.com>
2022-03-28 18:13:06 -0400

    just updated so our code is a bit cleaner

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,6 +1,4 @@
-import { start } from "repl";
-
-export function toRna(startingDnaLetter: string): string {
+export function toRna(startingDnaLetters: string): string {
 
   // define our object containing transcriptions
   const dnaToRna: any = {
@@ -10,22 +8,19 @@ export function toRna(startingDnaLetter: string): string {
     A: 'U'
   }
 
-  // error handling: we split startingDnaLetter into an
-  // array so we can iterate over each element in the string.
-  // if the letter we're checking in startingDnaLetter is in
-  // the dnaToRna object, do nothing and proceed.
-  // if it's not in the object, throw an error.
-  startingDnaLetter.split('').forEach((letter) => {
+  // error handling. If the letter we're checking
+  // is not in dnaToRna, throw an error
+  startingDnaLetters.split('').forEach((letter) => {
     if (dnaToRna[letter]) { }
     else {
       throw 'Invalid input DNA.'
     }
   })
 
-  // first we convert the startingDnaLetter string to an array
-  // using split(). Then, we create a new array of the mapped values. 
-  // Then, finally we use join() to combine it back into a string.
-  return startingDnaLetter.split('').map((value) => {
+  // our return statement splits startingDnaLetters
+  // into an array then creates a new array with the
+  // rna values. Then we use join to combine it into a string
+  return startingDnaLetters.split('').map((value) => {
     return dnaToRna[value];
   }).join('')
 }
