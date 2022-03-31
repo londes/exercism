28db06aa397364c303ac8ecafb6ee27ca94ff879
William Purcell <william@chartbeat.com>
2022-03-31 15:58:55 -0400

    added checks to ensure each letter is used

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -3,14 +3,22 @@ import { appendFile } from "fs";
 export function isPangram(testPangram: string): boolean {
   let alphabet: Array<string> = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+  let usedLetters: Array<string> = [];
+
   // remove whitespace
   let pangramCheck: Array<string> = testPangram.split('').filter(character => {
-    if (alphabet.includes(character))
+    if (alphabet.includes(character)) {
+      if (!usedLetters.includes(character)) {
+        usedLetters.push(character);
+      }
       return character;
+    }
   })
 
+  usedLetters.sort();
+
   pangramCheck.forEach(character => {
-    if (!alphabet.includes(character)) {
+    if (!alphabet.includes(character) || (alphabet != usedLetters)) {
       return false;
     }
   })
