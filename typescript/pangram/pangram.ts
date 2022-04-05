3028af6a36347d20befa60f82ee5c133c8a8b4be
William Purcell <william@chartbeat.com>
2022-04-05 12:47:17 -0400

    improved our pangram program

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -6,33 +6,10 @@ export function isPangram(testPangram: string): boolean {
   // in a test string
   let alphabet: Array<string> = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  let usedLetters: Array<string> = [];
+  // create comparison array from provided string
+  let testPangramChars: Array<string> = testPangram.toLowerCase().split('');
 
-  // remove whitespace, and push new letters to usedLetters.
-  // we don't need pangramCheck later, but this artifact
-  // may be useful to have in this program.
-  let pangramCheck: Array<string> = testPangram.split('').filter(character => {
-    if (alphabet.includes(character.toLowerCase())) {
-      if (!usedLetters.includes(character.toLowerCase())) {
-        usedLetters.push(character.toLowerCase());
-      }
-      return character.toLowerCase();
-    }
-  })
-
-  // sorts our usedLetters array a thru z
-  usedLetters.sort();
-
-  // use a function to check that alphabet matches the
-  // letters used in the test string. return the result.
-  return checkForEqualValues(alphabet, usedLetters);
-
-  // accepts: two arrays
-  // returns: boolean valye of whether or not they match
-  function checkForEqualValues(first: Array<string>, second: Array<string>): boolean {
-    for (let i = 0; i < first.length; i++) {
-      if (first[i] != second[i]) return false
-    }
-    return true;
-  }
+  // return true if every letter in the alphabet appears in
+  // the provided string
+  return alphabet.every(letter => testPangramChars.includes(letter));
 }
