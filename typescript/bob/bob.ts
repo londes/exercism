957c7469341ce4535b1b57e48328c592499e07d5
William Purcell <william@chartbeat.com>
2022-04-05 16:19:10 -0400

    bob passed but it is getting nitty gritty to pass all tests

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,3 +1,29 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function hey(message: string): string {
+
+  let messageArray: Array<string> = message.replaceAll(/\s/g, '').split('');
+
+  let lowerCaseLetters: number = 0;
+  let upperCaseLetters: number = 0;
+
+  messageArray.forEach(letter => {
+    if (letter == letter.toLowerCase())
+      lowerCaseLetters++;
+    else if (letter == letter.toUpperCase())
+      upperCaseLetters++;
+  })
+
+  let yellCheck: boolean = upperCaseLetters > lowerCaseLetters;
+
+
+
+  if (messageArray.length == 0) {
+    return 'Fine. Be that way!'
+  } else if (messageArray[messageArray.length - 1] == '?' && !yellCheck) {
+    return 'Sure.'
+  } else if (messageArray[messageArray.length - 1] == '?' && yellCheck) {
+    return 'Calm down, I know what I\'m doing!'
+  } else if (yellCheck) {
+    return 'Whoa, chill out!'
+  }
+  return 'Whatever.';
 }
