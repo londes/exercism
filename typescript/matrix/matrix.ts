6be601b461a7234040da511417bbd31970d37006
William Purcell <william@chartbeat.com>
2022-04-07 14:36:07 -0400

    just pushing this up before stepping away from this project for a bit

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,17 +1,34 @@
 export class Matrix {
-  matrix: Array<Array<string>>;
+  matrix: Array<Array<string>>
+  _rows: Array<number>
+  _columns: Array<number>
 
   constructor(matrixStarter: string) {
     this.matrix = this.createMatrix(matrixStarter);
+    this._rows = this.rows;
+    this._columns = this.columns
   }
 
-  get rows(): Array<string> {
-    console.log(this.matrix);
-    return [];
+  get rows(): Array<number> {
+    console.log('matrix: ' + this.matrix);
+
+    let rowsArray: Array<Array<string>> = this.matrix.slice();
+    console.log('rowsArray before: ' + rowsArray);
+    // let rowsNums: Array<number> = [];
+
+    // for (let i = 0; i < rowsArray.length; i++) {
+    //   for (let j = 0; j < rowsArray[i].length; j++) {
+    //     parseInt(rowsArray[i][j]);
+    //   }
+    // }
+
+    // console.log('rowsArray after: ' + rowsArray);
+
+    return [1, 2, 3]
   }
 
-  get columns(): Array<string> {
-    return [];
+  get columns(): Array<number> {
+    return [4, 5, 6];
   }
 
   createMatrix(arrayStarter: string): Array<Array<string>> {
