0336add2f9113f5cd9dfe4b392f9458306b9c423
William Purcell <william@chartbeat.com>
2022-04-11 15:15:55 -0400

    passes but I know this code isn't great

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -9,21 +9,26 @@ export class Matrix {
     this._columns = this.columns
   }
 
+  // our matrix is already sorted into rows, so just
+  // return the requested index value
   get rows(): Array<Array<number>> {
     return this.matrix
   }
 
+  // we have to rebuild the matrix by iterating into
+  // each column to push its values into an array, then
+  // push that column value array into our columns matrix
   get columns(): Array<Array<number>> {
     let newColumns: Array<Array<number>> = [];
 
     for (let i = 0; i < this.matrix.length; i++) {
       let oneColumn: Array<number> = [];
-      for (let j = 0; j < this.matrix[i].length; j++) {
+      for (let j = 0; j < this.matrix.length; j++) {
         oneColumn.push(this.matrix[j][i]);
       }
       newColumns.push(oneColumn);
     }
-    console.log('new columns: ' + newColumns);
+
     return newColumns;
   }
 
