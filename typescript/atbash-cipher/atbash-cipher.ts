7571f9386698650574456c694c33ac1c84af4071
William Purcell <william@chartbeat.com>
2022-08-14 22:06:23 -0400

    pushing some basic code, still have to deal with spaces and numbers

--- typescript/atbash-cipher/atbash-cipher.ts
+++ typescript/atbash-cipher/atbash-cipher.ts
@@ -1,5 +1,16 @@
-export function encode(plainText: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+import { kStringMaxLength } from "buffer"
+
+let alphabet: Array<string> = Array.from(Array(26)).map((e, i) => i + 65).map((x) => String.fromCharCode(x).toLowerCase())
+
+let cypher: Array<string> = Array.from(Array(26)).map((e, i) => i + 65).map((x) => String.fromCharCode(x).toLowerCase()).reverse()
+
+export function encode(plainText: string): string {
+  let cleanedText = plainText.split("").map((letter) => {
+    if (letter.match(/[a-z]/i))
+      return letter
+  })
+  console.log('cleaned Text: ' + cleanedText)
+  return ''
 }
 
 export function decode(cipherText: unknown): unknown {
