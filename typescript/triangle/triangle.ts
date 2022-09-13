95aea20e1833909af583d67e96f76a92b86aa7aa
William Purcell <william@chartbeat.com>
2022-09-12 21:46:03 -0400

    installed and taking a stab at an exercise

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -1,16 +1,23 @@
 export class Triangle {
-  constructor(...sides) {
-    throw new Error('Remove this statement and implement this function')
+  lengths: number[]
+
+  constructor(...sides: number[]) {
+    this.lengths = sides
   }
 
+  // all sides equal
   get isEquilateral() {
-    throw new Error('Remove this statement and implement this function')
+    let equal: any[] = this.lengths.map((a, b) => a == b)
+    console.log(equal)
+    return ''
   }
 
+  // at least two sides equal
   get isIsosceles() {
     throw new Error('Remove this statement and implement this function')
   }
 
+  // all sides different
   get isScalene() {
     throw new Error('Remove this statement and implement this function')
   }
