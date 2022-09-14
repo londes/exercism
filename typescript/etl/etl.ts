68d0d86727dc591b88adb459b2784f42482cb670
William Purcell <william@chartbeat.com>
2022-09-13 22:48:02 -0400

    slight improvement to etl

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -2,7 +2,7 @@ type Input = { [key: string]: string[] }
 type Output = { [key: string]: number }
 
 export function transform(input: Input): Output {
-  let output: { [key: string]: number } = {}
+  let output: Output = {}
   Object.entries(input).forEach(pair => {
     pair[1].forEach(value => {
       output[value.toLowerCase()] = Number(pair[0])
