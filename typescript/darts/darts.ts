bc34b9c40d9df69cb4611c14eabe02e7ce5c6157
William Purcell <williampurcell@Williams-MBP.home>
2023-10-21 18:21:21 +0200

    solved darts

--- typescript/darts/darts.ts
+++ typescript/darts/darts.ts
@@ -1,3 +1,11 @@
-export function score(x: unknown, y: unknown): unknown {
-  return 1
+export function score(x: number, y: number): number {
+  let distance: number = Math.sqrt(x ** 2 + y ** 2)
+  console.log('number is: ' + distance )
+  if (distance <= 1)
+    return 10
+  else if (distance <= 5)
+    return 5
+  else if (distance <= 10)
+    return 1
+  return 0
 }
